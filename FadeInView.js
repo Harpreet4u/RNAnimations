5803f29c0991b896fc47a90708be8a2ed67091a0
harpreet4u <happysinghsingh7@gmail.com>
2017-06-04 15:40:32 +0530

    FadeInView component with Animated.View fade animation

---
+++ FadeInView.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+import {
+  Animated
+} from 'react-native';
+
+class FadeInView extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      fadeAnim: new Animated.Value(0),
+    };
+  }
+
+  componentDidMount() {
+    Animated.timing(
+      this.state.fadeAnim,
+      {
+        toValue: 1,
+        duration: 2500
+      }
+    ).start();
+  }
+
+  _getStyles() {
+    return this.props.style ? this.props.style : {};
+  }
+
+  render() {
+    // For performance reasons always pass object
+    // instead of using spread operator.
+    let givenStyle = this._getStyles();
+    let propStyle = Object.assign({}, ...givenStyle,
+      {
+        opacity: this.state.fadeAnim
+      });
+    return (
+      <Animated.View
+        style={propStyle}>
+        {this.props.children}
+      </Animated.View>
+    );
+  }
+}
+module.exports = FadeInView;
