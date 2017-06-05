e8e92d7541528ed15cfbd2a757764e1347b340db
harpreet4u <happysinghsingh7@gmail.com>
2017-06-05 05:44:41 +0530

    Spring layout animation on box

--- index.android.js
+++ index.android.js
@@ -10,12 +10,33 @@ import {
   StyleSheet,
   Text,
   View,
-  Animated
+  Animated,
+  LayoutAnimation,
+  UIManager,
+  TouchableOpacity
 } from 'react-native';
 
 import FadeInView from './FadeInView';
 
 export default class MyAnimations extends Component {
+  constructor(props) {
+    super(props);
+    UIManager.setLayoutAnimationEnabledExperimental &&
+      UIManager.setLayoutAnimationEnabledExperimental(true);
+
+    this.state = {w: 100, h: 100};
+    this._onPress = this._onPress.bind(this);
+  }
+
+  componentWillMount() {
+    LayoutAnimation.spring();
+  }
+
+  _onPress() {
+    LayoutAnimation.spring();
+    this.setState({w: this.state.w + 15, h: this.state.h + 15});
+  }
+
   render() {
     return (
       <View style={styles.container}>
@@ -31,6 +52,14 @@ export default class MyAnimations extends Component {
             Shake or press menu button for dev menu
           </Text>
         </FadeInView>
+        <View style={[styles.box, {width: this.state.w, height: this.state.h}]} />
+        <TouchableOpacity onPress ={this._onPress}>
+          <View style={styles.button}>
+            <Text style={styles.buttonText}>
+              Press me!
+            </Text>
+          </View>
+        </TouchableOpacity>
       </View>
     );
   }
@@ -52,6 +81,21 @@ const styles = StyleSheet.create({
     textAlign: 'center',
     color: '#333333',
     marginBottom: 5,
+  },
+  box: {
+    margin: 10,
+    backgroundColor: 'orange'
+  },
+  button: {
+    width: 80,
+    height: 50,
+    margin: 5,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'red'
+  },
+  buttonText: {
+    color: 'white'
   },
 });
 
